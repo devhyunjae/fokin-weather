556ec0be265461f094a58f6514cca7d78473510c
Steve Hyunjae Park <dev.hyunjae.park@gmail.com>
2020-09-15 21:23:17 +0900

    Request permission and get Location

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import { Alert, View, Text } from 'react-native';
 import Loading from './views/Loading';
 
-const App = () => (
-  <Loading />
-);
+const App = () => {
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        await Location.requestPermissionsAsync();
+        const { coords: { longitude, latitude } } = await Location.getCurrentPositionAsync();
+        setLocation({
+          longitude,
+          latitude,
+        });
+      } catch (error) {
+        Alert('Cannot find your current location');
+      }
+    })();
+  }, []);
+
+  if (location) {
+    return (
+      <View>
+        <Text>
+          { location.longitude}
+        </Text>
+        <Text>
+          { location.latitude}
+        </Text>
+      </View>
+    );
+  }
+  return (
+    <Loading />
+  );
+};
 
 export default App;
