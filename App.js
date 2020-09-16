745b478778ced6315ad2eb0fecc4d3e5e1a48016
Steve Hyunjae Park <dev.hyunjae.park@gmail.com>
2020-09-16 10:16:22 +0900

    Add to request to open weather api

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
 import { Alert, View, Text } from 'react-native';
+import axios from 'axios';
 import Loading from './views/Loading';
 
 const App = () => {
@@ -14,6 +15,8 @@ const App = () => {
           longitude,
           latitude,
         });
+        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d33d09e9828067cf206834b39082d2a0`);
+        console.log('response', data);
       } catch (error) {
         Alert('Cannot find your current location');
       }
